a14c43c515b5e4c7cd03f39321f4e73b1a5e9868
Johannes Hettig <johannes.hettig@gmail.com>
2018-08-28 21:04:31 +0800

    Added more location

--- Map_course/scripts/mapsdata.js
+++ Map_course/scripts/mapsdata.js
@@ -1,34 +1,86 @@
- // Create a map variable
-  var map;
- // Complete the following function to initialize the map
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.979766, lng: -73.728107},
-      zoom: 13
-    });
-  // inside the initMap function we add a marker
-  // first specify the location of the marker
-    var home = {lat: 40.979766, lng: -73.728107};
+// Create a map variable
+var map;
 
+// Create a blank array for all the markers.
+var markers = [];
+
+// Complete the following function to initialize the map
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 40.979766, lng: -73.728107},
+  zoom: 13
+});
+// inside the initMap function we add a marker
+// first specify the locations of the marker
+// These are my favorite locations.
+var locations = [
+    {title: 'home', location: {lat: 40.9795154, lng: -73.72806439999999}},
+    {title: 'Work', location: {lat: 41.034806, lng: -74.1079578}},
+    {title: 'School', location: {lat: 41.012505, lng: -73.7344567}},
+    {title: 'Church', location: {lat: 41.022505, lng: -73.7344567}},
+    {title: 'Elevation_Burger', location: {lat: 41.032505, lng: -73.7344567}},
+    {title: 'Japanese', location: {lat: 41.042505, lng: -73.7344567}},
+    {title: 'Beerhouse', location: {lat: 41.052505, lng: -73.7344567}},
+    {title: 'Kiga', location: {lat: 41.062505, lng: -73.7344567}}
+    ];
+
+var largeInfoWindow = new google.maps.InfoWindow();
+
+// Create array for the marker label
+var label = ['A','B', 'C','D','E','F','G'];
+
+// loop through the locations to create multiple markers
+for (var i = 0; i < locations.length; i++) {
+    // get the title and the position from the local array
+    var position = locations[i].location;
+    var title = locations[i].title;
+    console.log(title);
+
+    // Create a marker for each location
     var marker = new google.maps.Marker({
-        position : home,
+        position : position,
         map : map,
-        label: "A",
-        title: 'Thats where I lived!'
+        label: label[i],
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
         });
-  // geting the position of the marker.
-    var m_lat = marker.getPosition().lat();
-    var m_lng = marker.getPosition().lng();
-    console.log(m_lat)
-    console.log(m_lng)
-  // Now we want to create an infowindow which pops up when the marker is clicked
-    var infowindow = new google.maps.InfoWindow({
-        content: "Here was out house where we spent 5 awesome years! Lat: " + m_lat + "  Long: " + m_lng
+
+    // append the marker in the predefined markers array
+    markers.push(marker);
+
+    // Create event listener for each marker
+    // passing in the marker, which is "this"
+    // and populate the Info Window specific to that marker.
+    // large Info Window is created below.
+    marker.addListener('click', function(){
+        populateInfoWindow(this, largeInfoWindow);
     });
-    // the marker function created above does have a event listener function
-    marker.addListener('click',function(){
-        // infowindow opens on the map on the marker
-        // if no marker is passed anchor point need to be created.
+
+}
+
+// This function populate the marker window when the marker is clicked.
+// the "this" marker and the "largeInfoWindow" is passed as argument
+function populateInfoWindow(marker, infowindow) {
+    // Check if infowindow is already open
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        console.log(marker.title);
+        infowindow.setContent('<div>' + marker.title + '</div>');
         infowindow.open(map, marker);
-    });
-  }
+        // Clear marker property if the window is closed
+        infowindow.addListener('closeclick', function(){
+            infowindow.marker = null;
+        });
+    }
+
+
+}
+
+// geting the position of the marker.
+var m_lat = markers[0].getPosition().lat();
+var m_lng = markers[0].getPosition().lng();
+console.log(m_lat)
+console.log(m_lng)
+
+}
